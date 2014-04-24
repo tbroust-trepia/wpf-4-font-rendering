380e5121898d70939fd734d8def5a58ae739dd90
Thomas Broust <tbroust-trepia@ventadis.fr>
2014-04-24 10:39:07 +0200

    Test projects works

---
+++ test_fonts_spacing_net35/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace test_fonts_spacing_net35
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
